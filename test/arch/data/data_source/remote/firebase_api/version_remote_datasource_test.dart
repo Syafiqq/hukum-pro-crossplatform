7e157428745b14505d9f61da70bc6f1e1791a566
msyafiqq <muhammad.syafiq@beautyfulminds.com.sg>
2021-05-18 18:29:10 +0700

    update version remote datasource test

--- test/arch/data/data_source/remote/firebase_api/version_remote_datasource_test.dart
+++ test/arch/data/data_source/remote/firebase_api/version_remote_datasource_test.dart
@@ -1,48 +1,218 @@
-// @dart=2.9
+import 'dart:async';
 
+import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_database/firebase_database.dart';
-import 'package:firebase_database_mocks/firebase_database_mocks.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:hukum_pro/arch/data/data_source/remote/impl/firebase_api.dart';
-import 'package:hukum_pro/arch/domain/entity/misc/version_entity.dart';
 
-void main() {
-  FirebaseDatabase firebaseDatabase;
-  FirebaseApi firebaseApi;
+import 'package:firebase_core_platform_interface/firebase_core_platform_interface.dart';
 
-  MockFirebaseDatabase.instance.reference().set(firebaseDataCorrect);
-  setUp(() {
-    firebaseDatabase = MockFirebaseDatabase.instance;
-    firebaseApi = FirebaseApi(firebaseDatabase);
-
-    expect(firebaseDatabase, isNotNull);
-    expect(firebaseApi, isNotNull);
-  });
-
-  test('test it should produce correct VersionEntity', () async {
-    VersionEntity version = await firebaseApi.getVersion();
-
-    expect(version, isNotNull);
+void initializeMethodChannel() {
+  TestWidgetsFlutterBinding.ensureInitialized();
+  MethodChannelFirebase.channel.setMockMethodCallHandler((call) async {
+    if (call.method == 'Firebase#initializeCore') {
+      return [
+        {
+          'name': '[DEFAULT]',
+          'options': {
+            'apiKey': '123',
+            'appId': '123',
+            'messagingSenderId': '123',
+            'projectId': '123',
+          },
+          'pluginConstants': {},
+        }
+      ];
+    }
+    if (call.method == 'Firebase#initializeApp') {
+      return {
+        'name': call.arguments['appName'],
+        'options': call.arguments['options'],
+        'pluginConstants': {},
+      };
+    }
+    return null;
   });
 }
 
-const firebaseDataCorrect = {
-  'versions_new': {
-    'v1': {
-      '1603971592286': {
-        'detail': {
-          'filenames': [
-            '1603971592286-1-1.json',
-            '1603971592286-2-1.json',
-            '1603971592286-3-1.json',
-            '1603971592286-4-1.json',
-            '1603971592286-5-1.json',
-            '1603971592286-6-1.json'
-          ]
-        },
-        'milis': 1603971592286,
-        'timestamp': '2020-10-29 18:39:52'
-      }
+void main() {
+  initializeMethodChannel();
+  late FirebaseApp app;
+
+  setUpAll(() async {
+    app = await Firebase.initializeApp(
+      name: 'testApp',
+      options: const FirebaseOptions(
+        appId: '1:1234567890:ios:42424242424242',
+        apiKey: '123',
+        projectId: '123',
+        messagingSenderId: '1234567890',
+      ),
+    );
+  });
+
+  group('$FirebaseDatabase', () {
+    const MethodChannel channel = MethodChannel(
+      'plugins.flutter.io/firebase_database',
+    );
+    int mockHandleId = 0;
+    final List<MethodCall> log = <MethodCall>[];
+
+    const String databaseURL = 'https://fake-database-url2.firebaseio.com';
+    late FirebaseDatabase database;
+
+    setUp(() async {
+      database = FirebaseDatabase(app: app, databaseURL: databaseURL);
+
+      channel.setMockMethodCallHandler((MethodCall methodCall) async {
+        log.add(methodCall);
+        switch (methodCall.method) {
+          case 'Query#observe':
+            return mockHandleId++;
+          case 'FirebaseDatabase#setPersistenceEnabled':
+            return true;
+          case 'FirebaseDatabase#setPersistenceCacheSizeBytes':
+            return true;
+          case 'DatabaseReference#runTransaction':
+            late Map<String, dynamic> updatedValue;
+            Future<void> simulateEvent(
+                int transactionKey, final MutableData mutableData) async {
+              await ServicesBinding.instance!.defaultBinaryMessenger
+                  .handlePlatformMessage(
+                channel.name,
+                channel.codec.encodeMethodCall(
+                  MethodCall(
+                    'DoTransaction',
+                    <String, dynamic>{
+                      'transactionKey': transactionKey,
+                      'snapshot': <String, dynamic>{
+                        'key': mutableData.key,
+                        'value': mutableData.value,
+                      },
+                    },
+                  ),
+                ),
+                (data) {
+                  updatedValue = channel.codec
+                      .decodeEnvelope(data!)['value']
+                      .cast<String, dynamic>();
+                },
+              );
+            }
+
+            await simulateEvent(
+                0,
+                MutableData.private(<String, dynamic>{
+                  'key': 'fakeKey',
+                  'value': <String, dynamic>{'fakeKey': 'fakeValue'},
+                }));
+
+            return <String, dynamic>{
+              'error': null,
+              'committed': true,
+              'snapshot': <String, dynamic>{
+                'key': 'fakeKey',
+                'value': updatedValue
+              },
+              'childKeys': ['fakeKey']
+            };
+          default:
+            return null;
+        }
+      });
+      log.clear();
+    });
+
+    group('$Query', () {
+      test('observing value events', () async {
+        mockHandleId = 87;
+        const String path = 'foo';
+        final Query query = database.reference().child(path);
+        Future<void> simulateEvent(String value) async {
+          await ServicesBinding.instance!.defaultBinaryMessenger
+              .handlePlatformMessage(
+                  channel.name,
+                  channel.codec.encodeMethodCall(
+                    MethodCall('Event', <String, dynamic>{
+                      'handle': 87,
+                      'snapshot': <String, dynamic>{
+                        'key': path,
+                        'value': value,
+                      },
+                    }),
+                  ),
+                  (_) {});
+        }
+
+        final AsyncQueue<Event> events = AsyncQueue<Event>();
+
+        // Subscribe and allow subscription to complete.
+        final StreamSubscription<Event> subscription =
+            query.onValue.listen(events.add);
+        await Future<void>.delayed(const Duration());
+
+        await simulateEvent('1');
+        await simulateEvent('2');
+        final Event event1 = await events.remove();
+        final Event event2 = await events.remove();
+        expect(event1.snapshot.key, path);
+        expect(event1.snapshot.value, '1');
+        expect(event2.snapshot.key, path);
+        expect(event2.snapshot.value, '2');
+
+        // Cancel subscription and allow cancellation to complete.
+        await subscription.cancel();
+        await Future<void>.delayed(const Duration());
+
+        expect(
+          log,
+          <Matcher>[
+            isMethodCall(
+              'Query#observe',
+              arguments: <String, dynamic>{
+                'app': app.name,
+                'databaseURL': databaseURL,
+                'path': path,
+                'parameters': <String, dynamic>{},
+                'eventType': '_EventType.value',
+              },
+            ),
+            isMethodCall(
+              'Query#removeObserver',
+              arguments: <String, dynamic>{
+                'app': app.name,
+                'databaseURL': databaseURL,
+                'path': path,
+                'parameters': <String, dynamic>{},
+                'handle': 87,
+              },
+            ),
+          ],
+        );
+      });
+    });
+  });
+}
+
+/// Queue whose remove operation is asynchronous, awaiting a corresponding add.
+class AsyncQueue<T> {
+  Map<int, Completer<T>> _completers = <int, Completer<T>>{};
+  int _nextToRemove = 0;
+  int _nextToAdd = 0;
+
+  void add(T element) {
+    _completer(_nextToAdd++).complete(element);
+  }
+
+  Future<T> remove() {
+    return _completer(_nextToRemove++).future;
+  }
+
+  Completer<T> _completer(int index) {
+    if (_completers.containsKey(index)) {
+      return _completers.remove(index)!;
+    } else {
+      return _completers[index] = Completer<T>();
     }
   }
-};
+}
