6491d4a61973c4b9b4413952ffc87d5a5d7e2153
msyafiqq <muhammad.syafiq@beautyfulminds.com.sg>
2021-05-18 21:09:38 +0700

    fixing flow

--- lib/arch/data/data_source/remote/impl/firebase_api.dart
+++ lib/arch/data/data_source/remote/impl/firebase_api.dart
@@ -2,6 +2,7 @@ import 'package:firebase_database/firebase_database.dart';
 import 'package:hukum_pro/arch/data/data_source/remote/contract/version_remote_datasource.dart';
 import 'package:hukum_pro/arch/domain/entity/misc/version_entity.dart';
 import 'package:hukum_pro/common/exception/built_in.dart';
+import 'package:flinq/flinq.dart';
 
 class FirebaseApi implements VersionRemoteDatasource {
   FirebaseDatabase _database;
@@ -10,19 +11,18 @@ class FirebaseApi implements VersionRemoteDatasource {
 
   @override
   Future<VersionEntity> getVersion() async {
-    Map? snapshot = (await _database
-            .reference()
-            .child('versions_new/v1')
-            .orderByKey()
-            .limitToLast(1)
-            .once())
-        .value
-        ?.values
-        ?.first as Map?;
-    if (snapshot == null) throw DataNotExistsException(null);
+    var snapshot = await _database
+        .reference()
+        .child('versions_new/v1')
+        .orderByKey()
+        .limitToLast(1)
+        .once();
+    var versions = snapshot.value?.values as Iterable?;
+    var rawVersion = versions?.firstOrNull as Map?;
+    if (rawVersion == null) throw DataNotExistsException(null);
     try {
-      VersionEntity versionEntity = VersionEntity.fromJson(snapshot);
-      return versionEntity;
+      VersionEntity version = VersionEntity.fromJson(rawVersion);
+      return version;
     } on Exception catch (e) {
       throw ParseFailedException(VersionEntity, e);
     }
