884c4c6c34e8def71d72cee133a8f510e577c7a7
msyafiqq <muhammad.syafiq@beautyfulminds.com.sg>
2021-05-19 12:51:34 +0700

    Fixin repository

--- lib/arch/data/repository/version_repository_impl.dart
+++ lib/arch/data/repository/version_repository_impl.dart
@@ -2,13 +2,12 @@ import 'package:hukum_pro/arch/data/data_source/remote/contract/version_remote_d
 import 'package:hukum_pro/arch/domain/entity/misc/version_entity.dart';
 import 'package:hukum_pro/arch/domain/repository/version_repository.dart';
 
-class VersionRepositoryImpl extends VersionRepository {
+class VersionRepositoryImpl implements VersionRepository {
   VersionRemoteDatasource remoteDatasource;
 
-  VersionRepositoryImpl(this.remoteDatasource)
+  VersionRepositoryImpl(this.remoteDatasource);
 
   @override
-  Future<VersionEntity?> fetchFromRemote() async {
-    await remoteDatasource.getVersion();
-  }
+  Future<VersionEntity> fetchFromRemote() async =>
+      await remoteDatasource.getVersion();
 }
--- lib/arch/domain/repository/version_repository.dart
+++ lib/arch/domain/repository/version_repository.dart
@@ -1,5 +1,5 @@
 import 'package:hukum_pro/arch/domain/entity/misc/version_entity.dart';
 
 abstract class VersionRepository {
-  Future<VersionEntity?> fetchFromRemote();
+  Future<VersionEntity> fetchFromRemote();
 }
