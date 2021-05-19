6c4b8f17d6b2a52ac7da908ff2f7ce301cc809af
msyafiqq <muhammad.syafiq@beautyfulminds.com.sg>
2021-05-19 12:06:58 +0700

    enhance version entity test

--- test/arch/domain/entity/json/version_entity_test.dart
+++ test/arch/domain/entity/json/version_entity_test.dart
@@ -5,7 +5,6 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
-import 'dart:convert';
 
 import 'package:flutter_test/flutter_test.dart';
 import 'package:hukum_pro/arch/domain/entity/misc/version_entity.dart';
@@ -14,108 +13,66 @@ void main() {
   group('$VersionEntity', () {
     group('fromJson', () {
       test('success parse', () {
-        String json = """
-          {
-            "detail": {
-              "filenames": [
-                "1603971592286-1-1.json",
-                "1603971592286-2-1.json",
-                "1603971592286-3-1.json",
-                "1603971592286-4-1.json",
-                "1603971592286-5-1.json",
-                "1603971592286-6-1.json"
-              ]
-            },
-            "milis": 1603971592286,
-            "timestamp": "2020-10-29 18:39:52"
-          }
-          """;
-        Map<String, dynamic>? jsonMap = jsonDecode(json);
-        VersionEntity? entity;
-        if (jsonMap != null) {
-          entity = VersionEntity.fromJson(jsonMap);
-        }
+        var jsonMap = {
+          'detail': {
+            'filenames': [
+              '1603971592286-1-1.json',
+              '1603971592286-2-1.json',
+              '1603971592286-3-1.json',
+              '1603971592286-4-1.json',
+              '1603971592286-5-1.json',
+              '1603971592286-6-1.json'
+            ]
+          },
+          'milis': 1603971592286,
+          'timestamp': '2020-10-29 18:39:52'
+        };
+        var entity = VersionEntity.fromJson(jsonMap);
         expect(jsonMap, isNotNull);
         expect(entity, isNotNull);
-        expect(entity?.detail, isNotNull);
-        expect(entity?.milis, isNotNull);
-        expect(entity?.timestamp, isNotNull);
+        expect(entity.detail, isNotNull);
+        expect(entity.milis, isNotNull);
+        expect(entity.timestamp, isNotNull);
         print(entity);
       });
 
       test('success parse from empty json', () {
-        String json = """
-      {
-      }
-      """;
-        Map<String, dynamic>? jsonMap = jsonDecode(json);
-        VersionEntity? entity;
-        if (jsonMap != null) {
-          entity = VersionEntity.fromJson(jsonMap);
-        }
+        var jsonMap = {};
+        var entity = VersionEntity.fromJson(jsonMap);
         expect(jsonMap, isNotNull);
         expect(entity, isNotNull);
-        expect(entity?.detail, isNull);
-        expect(entity?.milis, isNull);
-        expect(entity?.timestamp, isNull);
+        expect(entity.detail, isNull);
+        expect(entity.milis, isNull);
+        expect(entity.timestamp, isNull);
         print(entity);
       });
 
       test('success parse from non related json', () {
-        String json = """
-          {
-            "a": 1,
-            "b": 2,
-            "c": 4
-          }
-          """;
-        Map<String, dynamic>? jsonMap = jsonDecode(json);
-        VersionEntity? entity;
-        if (jsonMap != null) {
-          entity = VersionEntity.fromJson(jsonMap);
-        }
+        var jsonMap = {'a': 1, 'b': 2, 'c': 4};
+        var entity = VersionEntity.fromJson(jsonMap);
         expect(jsonMap, isNotNull);
         expect(entity, isNotNull);
-        expect(entity?.detail, isNull);
-        expect(entity?.milis, isNull);
-        expect(entity?.timestamp, isNull);
+        expect(entity.detail, isNull);
+        expect(entity.milis, isNull);
+        expect(entity.timestamp, isNull);
         print(entity);
       });
 
       test('success parse from null value', () {
-        String json = """
-          {
-            "detail": null,
-            "milis": null,
-            "timestamp": null
-          }
-          """;
-        Map<String, dynamic>? jsonMap = jsonDecode(json);
-        VersionEntity? entity;
-        if (jsonMap != null) {
-          entity = VersionEntity.fromJson(jsonMap);
-        }
+        var jsonMap = {'detail': null, 'milis': null, 'timestamp': null};
+        var entity = VersionEntity.fromJson(jsonMap);
         expect(jsonMap, isNotNull);
         expect(entity, isNotNull);
-        expect(entity?.detail, isNull);
-        expect(entity?.milis, isNull);
-        expect(entity?.timestamp, isNull);
+        expect(entity.detail, isNull);
+        expect(entity.milis, isNull);
+        expect(entity.timestamp, isNull);
         print(entity);
       });
 
       test('failed parse from incorrect data type', () {
-        String json = """
-          {
-            "detail": 1,
-            "milis": 2,
-            "timestamp": 3
-          }
-          """;
-        Map<String, dynamic>? jsonMap = jsonDecode(json);
-        if (jsonMap != null) {
-          expect(() => VersionEntity.fromJson(jsonMap),
-              throwsA(predicate((e) => e is TypeError)));
-        }
+        var jsonMap = {'detail': 1, 'milis': 2, 'timestamp': 3};
+        expect(() => VersionEntity.fromJson(jsonMap),
+            throwsA(predicate((e) => e is TypeError)));
       });
     });
     group('toJson', () {});
