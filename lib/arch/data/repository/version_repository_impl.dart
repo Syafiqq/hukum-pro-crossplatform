bb1dd9caab787c9d15617a97dcfb5eced127cc0e
msyafiqq <muhammad.syafiq@beautyfulminds.com.sg>
2021-05-19 16:10:28 +0700

    fixing repository

--- lib/arch/data/repository/version_repository_impl.dart
+++ lib/arch/data/repository/version_repository_impl.dart
@@ -1,13 +1,23 @@
+import 'package:hukum_pro/arch/data/data_source/local/cache/contract/version_cache_datasource.dart';
 import 'package:hukum_pro/arch/data/data_source/remote/contract/version_remote_datasource.dart';
 import 'package:hukum_pro/arch/domain/entity/misc/version_entity.dart';
 import 'package:hukum_pro/arch/domain/repository/version_repository.dart';
 
 class VersionRepositoryImpl implements VersionRepository {
   VersionRemoteDatasource remoteDatasource;
+  VersionCacheDatasource cacheDatasource;
 
-  VersionRepositoryImpl(this.remoteDatasource);
+  VersionRepositoryImpl(this.remoteDatasource, this.cacheDatasource);
 
   @override
   Future<VersionEntity> fetchFromRemote() async =>
       await remoteDatasource.getVersion();
+
+  @override
+  Future<VersionEntity?> fetchFromLocal() async =>
+      await cacheDatasource.getVersion();
+
+  @override
+  Future<void> saveToLocal(VersionEntity version) async =>
+      await cacheDatasource.setVersion(version);
 }
--- lib/arch/domain/repository/version_repository.dart
+++ lib/arch/domain/repository/version_repository.dart
@@ -2,4 +2,8 @@ import 'package:hukum_pro/arch/domain/entity/misc/version_entity.dart';
 
 abstract class VersionRepository {
   Future<VersionEntity> fetchFromRemote();
+
+  Future<VersionEntity?> fetchFromLocal();
+
+  Future<void> saveToLocal(VersionEntity version);
 }
