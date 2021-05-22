f0416b0c4f4c42af2e9e1404517da8cbd617cf79
msyafiqq <muhammad.syafiq@beautyfulminds.com.sg>
2021-05-22 19:00:17 +0700

    add law menu order local datasource

--- lib/arch/data/data_source/local/contract/law_menu_order_local_datasource.dart
+++ lib/arch/data/data_source/local/contract/law_menu_order_local_datasource.dart
@@ -1,6 +1,6 @@
 import 'package:hukum_pro/arch/domain/entity/law/law_menu_order_entity.dart';
 
-abstract class VersionLocalDatasource {
+abstract class LawMenuOrderLocalDatasource {
   Future<List<LawMenuOrderEntity>> getMenus();
 
   Future<void> setMenus(List<LawMenuOrderEntity> menus);
