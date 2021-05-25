6cb8091cfd4abcd49cd989271fa30100b48f2e06
msyafiqq <muhammad.syafiq@beautyfulminds.com.sg>
2021-05-25 20:06:07 +0700

    Add decode bulk law

--- lib/arch/data/data_source/local/contract/bulk_laws_local_datasource.dart
+++ lib/arch/data/data_source/local/contract/bulk_laws_local_datasource.dart
@@ -1,5 +1,9 @@
 import 'dart:io';
 
+import 'package:hukum_pro/arch/domain/entity/law/law_entity.dart';
+
 abstract class BulkLawsLocalDatasource {
   Future<List<File>> getBulkLawDiskReferences(String id, List<String> names);
+
+  Future<List<LawEntity>> decodeBulkLaw(File file);
 }
--- lib/arch/data/data_source/local/impl/disk_path_provider.dart
+++ lib/arch/data/data_source/local/impl/disk_path_provider.dart
@@ -1,6 +1,8 @@
+import 'dart:convert';
 import 'dart:io';
 
 import 'package:hukum_pro/arch/data/data_source/local/contract/bulk_laws_local_datasource.dart';
+import 'package:hukum_pro/arch/domain/entity/law/law_entity.dart';
 import 'package:hukum_pro/common/exception/built_in.dart';
 import 'package:path_provider/path_provider.dart';
 
@@ -15,5 +17,25 @@ class DiskPathProvider implements BulkLawsLocalDatasource {
     } on MissingPlatformDirectoryException catch (e) {
       throw DataLocationNotFoundException(e, null);
     }
+  }
+
+  @override
+  Future<List<LawEntity>> decodeBulkLaw(File file) async {
+    var lawsString = await file.readAsString();
+    var lawsMap = jsonDecode(lawsString) as List<Map>?;
+    if (lawsMap == null) {
+      throw ParseFailedException(List, null, null);
+    }
+    var law = <LawEntity>[];
+
+    for (var rawMenu in lawsMap) {
+      try {
+        var menu = LawEntity.fromJson(rawMenu);
+        law.add(menu);
+      } on TypeError catch (e) {
+        throw ParseFailedException(LawEntity, null, e);
+      }
+    }
+    return law;
   }
 }
