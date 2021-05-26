f0427f79f737d3ed9009e6c9c8b639fb51f8f65e
msyafiqq <muhammad.syafiq@beautyfulminds.com.sg>
2021-05-26 12:37:49 +0700

    add hive database storage

--- lib/arch/data/data_source/local/impl/hive_database_storage.dart
+++ lib/arch/data/data_source/local/impl/hive_database_storage.dart
@@ -1,16 +1,15 @@
+import 'package:hive/hive.dart';
 import 'package:hukum_pro/arch/data/data_source/local/contract/law_local_datasource.dart';
 import 'package:hukum_pro/arch/domain/entity/law/law_entity.dart';
 
 class HiveDatabaseStorage implements LawLocalDatasource {
-  @override
-  Future<void> addLaws(List<LawEntity> laws) {
-    // TODO: implement addLaws
-    throw UnimplementedError();
-  }
+  HiveInterface hive;
+
+  HiveDatabaseStorage(this.hive) {}
 
   @override
-  Future<void> clear() {
-    // TODO: implement clear
-    throw UnimplementedError();
-  }
+  Future<void> addLaws(List<LawEntity> laws) async {}
+
+  @override
+  Future<void> clear() async {}
 }
