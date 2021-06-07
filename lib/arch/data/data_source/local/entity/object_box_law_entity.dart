3a6bd8e2699a2d9764c92fa59d2631df25c81213
msyafiqq <muhammad.syafiq@beautyfulminds.com.sg>
2021-06-07 21:18:24 +0700

    add object box law entity

---
+++ lib/arch/data/data_source/local/entity/object_box_law_entity.dart
@@ -0,0 +1,31 @@
+import 'package:equatable/equatable.dart';
+import 'package:objectbox/objectbox.dart';
+
+import 'law_entity.dart';
+
+@Entity()
+// ignore: must_be_immutable
+class ObjectBoxLawEntity extends Equatable implements LawEntity {
+  late int id;
+  late String remoteId;
+  int? year;
+  String? no;
+  String? description;
+  String? status;
+  String? reference;
+  String? category;
+  DateTime? dateCreated;
+
+  @override
+  List<Object?> get props => [
+        id,
+        remoteId,
+        year,
+        no,
+        description,
+        status,
+        reference,
+        category,
+        dateCreated
+      ];
+}
