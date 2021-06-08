5ff7ed15333db369c4b78af65c2c2726972619dc
msyafiqq <muhammad.syafiq@beautyfulminds.com.sg>
2021-06-08 07:12:33 +0700

    add object box database storage

---
+++ lib/arch/data/data_source/local/impl/object_box_database_storage.dart
@@ -0,0 +1,22 @@
+import 'package:hukum_pro/arch/data/data_source/local/contract/law_local_datasource.dart';
+import 'package:hukum_pro/arch/data/data_source/local/entity/object_box_law_entity.dart';
+import 'package:hukum_pro/arch/domain/entity/law/law_entity.dart';
+import 'package:objectbox/objectbox.dart';
+
+class ObjectBoxDatabaseStorage implements LawLocalDatasource {
+  late Store store;
+
+  ObjectBoxDatabaseStorage() {}
+
+  @override
+  Future<void> addLaws(List<LawEntity> laws) async {
+
+  }
+
+  @override
+  Future<void> clear() async {
+    var lawBox = store.box<ObjectBoxLawEntity>();
+    lawBox.removeAll();
+    store.close();
+  }
+}
