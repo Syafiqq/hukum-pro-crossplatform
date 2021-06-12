85a1e81629896fb60fd0abb4eae8a0b980dbe0c0
msyafiqq <muhammad.syafiq@beautyfulminds.com.sg>
2021-06-12 20:38:42 +0700

    add setter to store

--- lib/arch/data/data_source/local/contract/law_local_datasource.dart
+++ lib/arch/data/data_source/local/contract/law_local_datasource.dart
@@ -1,6 +1,8 @@
 import 'package:hukum_pro/arch/data/data_source/local/entity/law_entity.dart';
+import 'package:hukum_pro/objectbox.g.dart';
 
 abstract class LawLocalDatasource {
+  set store(Store store) {}
   Future<void> clear();
   Future<void> addLaws(List<LawEntity> laws);
 }
