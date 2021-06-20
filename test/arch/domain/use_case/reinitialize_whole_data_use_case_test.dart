18485456355fe78baa89f8ce1538a05fecb3d291
msyafiqq <muhammad.syafiq@beautyfulminds.com.sg>
2021-06-20 13:05:00 +0700

    fixing test

--- test/arch/domain/use_case/reinitialize_whole_data_use_case_test.dart
+++ test/arch/domain/use_case/reinitialize_whole_data_use_case_test.dart
@@ -10,6 +10,7 @@ import 'package:hukum_pro/arch/domain/repository/law_year_repository.dart';
 import 'package:hukum_pro/arch/domain/use_case/check_version_first_time_use_case.dart';
 import 'package:hukum_pro/arch/domain/use_case/reinitialize_whole_data_use_case.dart';
 import 'package:hukum_pro/arch/domain/use_case/reinitialize_whole_data_use_case_impl.dart';
+import 'package:hukum_pro/common/exception/built_in.dart';
 import 'package:hukum_pro/common/exception/defined_exception.dart';
 import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
@@ -116,6 +117,183 @@ void main() {
               .having((e) => e.count, 'count', 1)
         ],
       ]);
+    });
+
+    test('should execute the use case with minimum file execution', () async {
+      when(mockBulkLawsRepository.getFileReference(any, any))
+          .thenAnswer((_) => Future.value([File('1'), File('2')]));
+      when(mockBulkLawsRepository.downloadLaw(any, any))
+          .thenAnswer((_) => Future.value(null));
+      when(mockBulkLawsRepository.decodeFile(
+              argThat(isA<File>().having((e) => e.path, 'path', '1'))))
+          .thenAnswer((_) => Future.value(
+              [LawEntity(1, '1', 1, null, null, null, null, null, null)]));
+      when(mockBulkLawsRepository.decodeFile(
+              argThat(isA<File>().having((e) => e.path, 'path', '2'))))
+          .thenAnswer((_) => Future.value(
+              [LawEntity(2, '2', 2, null, null, null, null, null, null)]));
+      when(mockLawRepository.deleteAll()).thenAnswer((_) => Future.value(null));
+      when(mockLawRepository.addAll(any)).thenAnswer((_) => Future.value(null));
+      when(mockLawYearRepository.deleteAll())
+          .thenAnswer((_) => Future.value(null));
+      when(mockLawYearRepository.addAll(any))
+          .thenAnswer((_) => Future.value(null));
+
+      await useCase.execute(VersionEntity(
+          VersionDetailEntity(['1', '2', '3', '4', '5']), 2, '3'));
+
+      verify(mockLawRepository.deleteAll()).called(1);
+      verify(mockLawYearRepository.deleteAll()).called(1);
+
+      expect(
+          verify(mockBulkLawsRepository.getFileReference(
+                  captureAny, captureAny))
+              .captured,
+          [
+            '2', // id
+            ['1', '2', '3', '4', '5'] // filenames
+          ]);
+
+      expect(
+          verify(mockBulkLawsRepository.downloadLaw(captureAny, captureAny))
+              .captured,
+          [
+            '1',
+            isA<File>().having((e) => e.path, 'path', '1'),
+            '2',
+            isA<File>().having((e) => e.path, 'path', '2')
+          ]);
+
+      expect(verify(mockBulkLawsRepository.decodeFile(captureAny)).captured, [
+        isA<File>().having((e) => e.path, 'path', '1'),
+        isA<File>().having((e) => e.path, 'path', '2')
+      ]);
+
+      expect(verify(mockLawRepository.addAll(captureAny)).captured, [
+        [
+          isA<LawEntity>()
+              .having((e) => e.id, 'id', 1)
+              .having((e) => e.remoteId, 'remoteId', '1')
+              .having((e) => e.year, 'year', 1)
+        ],
+        [
+          isA<LawEntity>()
+              .having((e) => e.id, 'id', 2)
+              .having((e) => e.remoteId, 'remoteId', '2')
+              .having((e) => e.year, 'year', 2)
+        ]
+      ]);
+
+      expect(verify(mockLawYearRepository.addAll(captureAny)).captured, [
+        [
+          isA<LawYearEntity>()
+              .having((e) => e.id, 'id', 1)
+              .having((e) => e.year, 'year', 1)
+              .having((e) => e.count, 'count', 1),
+          isA<LawYearEntity>()
+              .having((e) => e.id, 'id', 2)
+              .having((e) => e.year, 'year', 2)
+              .having((e) => e.count, 'count', 1)
+        ],
+      ]);
+    });
+
+    test('should execute the use case with minimum filename execution',
+        () async {
+      when(mockBulkLawsRepository.getFileReference(any, any)).thenAnswer((_) =>
+          Future.value(
+              [File('2'), File('1'), File('3'), File('4'), File('5')]));
+      when(mockBulkLawsRepository.downloadLaw(any, any))
+          .thenAnswer((_) => Future.value(null));
+      when(mockBulkLawsRepository.decodeFile(
+              argThat(isA<File>().having((e) => e.path, 'path', '1'))))
+          .thenAnswer((_) => Future.value(
+              [LawEntity(1, '1', 1, null, null, null, null, null, null)]));
+      when(mockBulkLawsRepository.decodeFile(
+              argThat(isA<File>().having((e) => e.path, 'path', '2'))))
+          .thenAnswer((_) => Future.value(
+              [LawEntity(2, '2', 2, null, null, null, null, null, null)]));
+      when(mockLawRepository.deleteAll()).thenAnswer((_) => Future.value(null));
+      when(mockLawRepository.addAll(any)).thenAnswer((_) => Future.value(null));
+      when(mockLawYearRepository.deleteAll())
+          .thenAnswer((_) => Future.value(null));
+      when(mockLawYearRepository.addAll(any))
+          .thenAnswer((_) => Future.value(null));
+
+      await useCase
+          .execute(VersionEntity(VersionDetailEntity(['1', '2']), 2, '3'));
+
+      verify(mockLawRepository.deleteAll()).called(1);
+      verify(mockLawYearRepository.deleteAll()).called(1);
+
+      expect(
+          verify(mockBulkLawsRepository.getFileReference(
+                  captureAny, captureAny))
+              .captured,
+          [
+            '2', // id
+            ['1', '2'] // filenames
+          ]);
+
+      expect(
+          verify(mockBulkLawsRepository.downloadLaw(captureAny, captureAny))
+              .captured,
+          [
+            '1',
+            isA<File>().having((e) => e.path, 'path', '2'),
+            '2',
+            isA<File>().having((e) => e.path, 'path', '1')
+          ]);
+
+      expect(verify(mockBulkLawsRepository.decodeFile(captureAny)).captured, [
+        isA<File>().having((e) => e.path, 'path', '2'),
+        isA<File>().having((e) => e.path, 'path', '1')
+      ]);
+
+      expect(verify(mockLawRepository.addAll(captureAny)).captured, [
+        [
+          isA<LawEntity>()
+              .having((e) => e.id, 'id', 2)
+              .having((e) => e.remoteId, 'remoteId', '2')
+              .having((e) => e.year, 'year', 2)
+        ],
+        [
+          isA<LawEntity>()
+              .having((e) => e.id, 'id', 1)
+              .having((e) => e.remoteId, 'remoteId', '1')
+              .having((e) => e.year, 'year', 1)
+        ]
+      ]);
+
+      expect(verify(mockLawYearRepository.addAll(captureAny)).captured, [
+        [
+          isA<LawYearEntity>()
+              .having((e) => e.id, 'id', 2)
+              .having((e) => e.year, 'year', 2)
+              .having((e) => e.count, 'count', 1),
+          isA<LawYearEntity>()
+              .having((e) => e.id, 'id', 1)
+              .having((e) => e.year, 'year', 1)
+              .having((e) => e.count, 'count', 1),
+        ],
+      ]);
+    });
+
+    test('thrown an error if milis are null', () async {
+      when(mockLawRepository.deleteAll()).thenAnswer((_) => Future.value(null));
+      when(mockLawYearRepository.deleteAll())
+          .thenAnswer((_) => Future.value(null));
+
+      await expectLater(useCase.execute(VersionEntity(null, null, null)),
+          throwsA(isA<DataFetchFailureException>()));
+
+      verify(mockLawRepository.deleteAll()).called(1);
+      verify(mockLawYearRepository.deleteAll()).called(1);
+
+      verifyNever(mockBulkLawsRepository.downloadLaw(any, any));
+      verifyNever(mockBulkLawsRepository.decodeFile(any));
+      verifyNever(mockLawRepository.addAll(any));
+      verifyNever(mockLawYearRepository.addAll(any));
     });
   });
 }
