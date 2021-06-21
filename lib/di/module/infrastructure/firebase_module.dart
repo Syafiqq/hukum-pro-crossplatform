12ac6220232a5dbf0e735be21efc3c311668ebbe
msyafiqq <muhammad.syafiq@beautyfulminds.com.sg>
2021-06-21 20:52:37 +0700

    add firebase storage

--- lib/di/module/infrastructure/firebase_module.dart
+++ lib/di/module/infrastructure/firebase_module.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_database/firebase_database.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:kiwi/kiwi.dart';
 
 class FirebaseModule {
@@ -19,6 +20,13 @@ class FirebaseModule {
       final app = Firebase.app();
       return FirebaseDatabase(
           app: app, databaseURL: 'https://hukumpro-serverproxy.firebaseio.com');
+    });
+
+    // FirebaseCloudStorage
+    container.registerSingleton((c) {
+      final app = Firebase.app();
+      return FirebaseStorage.instanceFor(
+          app: app, bucket: 'hukumpro-serverproxy.appspot.com');
     });
   }
 }
