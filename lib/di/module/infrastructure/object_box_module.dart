8e71d193c252cc8bed551e74edad38dae7cfb616
msyafiqq <muhammad.syafiq@beautyfulminds.com.sg>
2021-06-23 06:58:42 +0700

    Fixing container

--- lib/di/module/infrastructure/object_box_module.dart
+++ lib/di/module/infrastructure/object_box_module.dart
@@ -5,6 +5,7 @@ import 'package:hukum_pro/di/contract/object_resolver.dart';
 import 'package:hukum_pro/objectbox.g.dart';
 import 'package:kiwi/kiwi.dart';
 import 'package:objectbox/objectbox.dart';
+import 'package:path_provider/path_provider.dart';
 
 class ObjectBoxModule {
   static final ObjectBoxModule _singleton = ObjectBoxModule._internal();
@@ -17,11 +18,18 @@ class ObjectBoxModule {
 
   void build() {
     KiwiContainer container = KiwiContainer();
-    container.registerFactory<Store>((c) {
+    container.registerFactory<Future<Store>>((c) async {
       final platform = c<PlatformIdentifier>();
       var path = '';
       if (platform.isAndroid) {
+        final directory = await getExternalStorageDirectory();
+        if (directory == null) {
+          throw UnsupportedError('Platform is not supported');
+        }
+        path = '${directory.path}/db/';
       } else if (platform.isIOS) {
+        final directory = await getApplicationSupportDirectory();
+        path = '${directory.path}/db/';
       } else {
         throw UnsupportedError('Platform is not supported');
       }
