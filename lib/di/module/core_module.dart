12b0231496b9f57ccd4e1711196aa9d919f2ba78
msyafiqq <muhammad.syafiq@beautyfulminds.com.sg>
2021-06-22 21:36:57 +0700

    fixing core module

---
+++ lib/di/module/core_module.dart
@@ -0,0 +1,21 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_database/firebase_database.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:hukum_pro/di/contract/object_resolver.dart';
+import 'package:hukum_pro/di/impl/kiwi_object_resolver.dart';
+import 'package:kiwi/kiwi.dart';
+
+class CoreModule {
+  static final CoreModule _singleton = CoreModule._internal();
+
+  factory CoreModule() {
+    return _singleton;
+  }
+
+  CoreModule._internal();
+
+  void build() {
+    KiwiContainer container = KiwiContainer();
+    container.registerSingleton<ObjectResolver>((_) => KiwiObjectResolver());
+  }
+}
