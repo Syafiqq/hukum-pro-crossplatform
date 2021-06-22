c13d1c23cefa56cc10caa2cdc5f7e625565501f9
msyafiqq <muhammad.syafiq@beautyfulminds.com.sg>
2021-06-22 20:41:34 +0700

    add law menu order repository

---
+++ lib/arch/data/repository/law_menu_order_repository_impl.dart
@@ -0,0 +1,23 @@
+import 'package:hukum_pro/arch/data/data_source/local/contract/law_menu_order_local_datasource.dart';
+import 'package:hukum_pro/arch/data/data_source/remote/contract/law_menu_order_remote_datasource.dart';
+import 'package:hukum_pro/arch/domain/entity/law/law_menu_order_entity.dart';
+import 'package:hukum_pro/arch/domain/repository/law_menu_order_repository.dart';
+
+class LawMenuOrderRepositoryImpl implements LawMenuOrderRepository {
+  LawMenuOrderRemoteDatasource remoteDatasource;
+  LawMenuOrderLocalDatasource localDatasource;
+
+  LawMenuOrderRepositoryImpl(this.remoteDatasource, this.localDatasource);
+
+  @override
+  Future<List<LawMenuOrderEntity>> fetchFromLocal() async =>
+      localDatasource.getMenusOrEmpty();
+
+  @override
+  Future<List<LawMenuOrderEntity>> fetchFromServer() async =>
+      remoteDatasource.getMenus();
+
+  @override
+  Future<void> save(List<LawMenuOrderEntity> menus) async =>
+      localDatasource.setMenus(menus);
+}
