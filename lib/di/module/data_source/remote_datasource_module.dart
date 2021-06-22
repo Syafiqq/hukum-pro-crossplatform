e2cbc629f14e7acd3d9d3d6e39f594299297c1b9
msyafiqq <muhammad.syafiq@beautyfulminds.com.sg>
2021-06-22 08:00:24 +0700

    fixing remote datasource module

--- lib/di/module/data_source/remote_datasource_module.dart
+++ lib/di/module/data_source/remote_datasource_module.dart
@@ -1,7 +1,10 @@
 import 'package:firebase_database/firebase_database.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:hukum_pro/arch/data/data_source/remote/contract/bulk_laws_remote_datasource.dart';
 import 'package:hukum_pro/arch/data/data_source/remote/contract/law_menu_order_remote_datasource.dart';
 import 'package:hukum_pro/arch/data/data_source/remote/contract/version_remote_datasource.dart';
 import 'package:hukum_pro/arch/data/data_source/remote/impl/firebase_cloud_database.dart';
+import 'package:hukum_pro/arch/data/data_source/remote/impl/firebase_cloud_storage.dart';
 import 'package:kiwi/kiwi.dart';
 
 class RemoteDatasourceModule {
@@ -18,13 +21,15 @@ class RemoteDatasourceModule {
     final KiwiContainer container = KiwiContainer();
     container
         .registerSingleton((c) => FirebaseCloudDatabase(c<FirebaseDatabase>()));
+    container
+        .registerSingleton((c) => FirebaseCloudStorage(c<FirebaseStorage>()));
+
     container.registerSingleton<VersionRemoteDatasource>(
         (c) => c<FirebaseCloudDatabase>());
     container.registerSingleton<LawMenuOrderRemoteDatasource>(
         (c) => c<FirebaseCloudDatabase>());
-  }
-}
 
-abstract class RemoteDatasourceInjectorModule {
-  void build();
+    container.registerSingleton<BulkLawsRemoteDatasource>(
+        (c) => c<FirebaseCloudStorage>());
+  }
 }
