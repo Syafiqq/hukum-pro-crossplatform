a75f49188d2c4b0fbb85fbc9ae476e321e20c8fc
msyafiqq <muhammad.syafiq@beautyfulminds.com.sg>
2021-06-22 08:44:13 +0700

    finishing local datasource

--- lib/di/root_injector.dart
+++ lib/di/root_injector.dart
@@ -1,5 +1,7 @@
+import 'package:hukum_pro/di/module/data_source/local_datasource_module.dart';
 import 'package:hukum_pro/di/module/data_source/remote_datasource_module.dart';
 import 'package:hukum_pro/di/module/infrastructure/firebase_module.dart';
+import 'package:hukum_pro/di/module/infrastructure/shared_preferences_module.dart';
 
 class RootInjector {
   static final RootInjector _singleton = RootInjector._internal();
@@ -12,6 +14,8 @@ class RootInjector {
 
   void build() {
     FirebaseModule().build();
+    SharedPreferencesModule().build();
     RemoteDatasourceModule().build();
+    LocalDatasourceModule().build();
   }
 }
