46256da7382f2bb2b96c5b30702a01eacb3bd242
msyafiqq <muhammad.syafiq@beautyfulminds.com.sg>
2021-06-22 08:08:08 +0700

    add check kiwi

--- lib/test_main.dart
+++ lib/test_main.dart
@@ -4,7 +4,11 @@ import 'package:flutter/material.dart';
 
 // Import the firebase_core plugin
 import 'package:firebase_core/firebase_core.dart';
+import 'package:hukum_pro/arch/data/data_source/remote/contract/bulk_laws_remote_datasource.dart';
+import 'package:hukum_pro/arch/data/data_source/remote/contract/law_menu_order_remote_datasource.dart';
+import 'package:hukum_pro/arch/data/data_source/remote/contract/version_remote_datasource.dart';
 import 'package:hukum_pro/arch/data/data_source/remote/impl/firebase_cloud_database.dart';
+import 'package:hukum_pro/arch/data/data_source/remote/impl/firebase_cloud_storage.dart';
 import 'package:hukum_pro/di/root_injector.dart';
 import 'package:kiwi/kiwi.dart';
 
@@ -34,6 +38,8 @@ class _AppState extends State<App> {
 
     // DI
     RootInjector().build();
+
+    await Future.delayed(Duration(seconds: 1));
   }
 
   @override
@@ -53,10 +59,7 @@ class _AppState extends State<App> {
           print("hasData = ${snapshot.hasData}");
           print(snapshot.data);
           print(Firebase.app());
-          KiwiContainer container = KiwiContainer();
-          print(container.resolve<FirebaseDatabase>());
-          print(container.resolve<FirebaseStorage>());
-          print(container.resolve<FirebaseCloudDatabase>());
+          checkKiwi();
         }
 
         // Otherwise, show something whilst waiting for initialization to complete
@@ -76,5 +79,31 @@ class _AppState extends State<App> {
         );
       },
     );
+  }
+
+  void checkKiwi() {
+    KiwiContainer container = KiwiContainer();
+    print('checkKiwi');
+    checkObject(Firebase.app());
+    checkObject(container.resolve<FirebaseApp>());
+    checkObject(container.resolve<FirebaseApp>());
+    checkObject(container.resolve<FirebaseDatabase>());
+    checkObject(container.resolve<FirebaseDatabase>());
+    checkObject(container.resolve<FirebaseStorage>());
+    checkObject(container.resolve<FirebaseStorage>());
+    checkObject(container.resolve<FirebaseCloudDatabase>());
+    checkObject(container.resolve<FirebaseCloudDatabase>());
+    checkObject(container.resolve<VersionRemoteDatasource>());
+    checkObject(container.resolve<VersionRemoteDatasource>());
+    checkObject(container.resolve<LawMenuOrderRemoteDatasource>());
+    checkObject(container.resolve<LawMenuOrderRemoteDatasource>());
+    checkObject(container.resolve<FirebaseCloudStorage>());
+    checkObject(container.resolve<FirebaseCloudStorage>());
+    checkObject(container.resolve<BulkLawsRemoteDatasource>());
+    checkObject(container.resolve<BulkLawsRemoteDatasource>());
+  }
+
+  void checkObject(Object x) {
+    print("${x.hashCode} - $x");
   }
 }
