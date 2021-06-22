ffd320513c9e78200a73c04c38863d3a90f44de6
msyafiqq <muhammad.syafiq@beautyfulminds.com.sg>
2021-06-22 21:18:40 +0700

    fixing objectbox module

--- lib/di/root_injector.dart
+++ lib/di/root_injector.dart
@@ -3,6 +3,8 @@ import 'package:hukum_pro/di/module/data_source/remote_datasource_module.dart';
 import 'package:hukum_pro/di/module/infrastructure/firebase_module.dart';
 import 'package:hukum_pro/di/module/infrastructure/shared_preferences_module.dart';
 
+import 'module/infrastructure/object_box_module.dart';
+
 class RootInjector {
   static final RootInjector _singleton = RootInjector._internal();
 
@@ -15,6 +17,7 @@ class RootInjector {
   void build() {
     FirebaseModule().build();
     SharedPreferencesModule().build();
+    ObjectBoxModule().build();
     RemoteDatasourceModule().build();
     LocalDatasourceModule().build();
   }
