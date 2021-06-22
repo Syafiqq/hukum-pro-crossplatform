60d23a0a538fb850bf47a247558b7604b3eed539
msyafiqq <muhammad.syafiq@beautyfulminds.com.sg>
2021-06-23 06:35:45 +0700

    add platform identiier

--- lib/di/module/core_module.dart
+++ lib/di/module/core_module.dart
@@ -1,6 +1,8 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:firebase_storage/firebase_storage.dart';
+import 'package:hukum_pro/arch/infrastructure/app/flutter_platform_identifier.dart';
+import 'package:hukum_pro/arch/infrastructure/app/platform_identifier.dart';
 import 'package:hukum_pro/di/contract/object_resolver.dart';
 import 'package:hukum_pro/di/impl/kiwi_object_resolver.dart';
 import 'package:kiwi/kiwi.dart';
@@ -17,5 +19,7 @@ class CoreModule {
   void build() {
     KiwiContainer container = KiwiContainer();
     container.registerSingleton<ObjectResolver>((_) => KiwiObjectResolver());
+    container.registerSingleton<PlatformIdentifier>(
+        (_) => FlutterPlatformIdentifier());
   }
 }
