dc981817fcb9a4b74bdad71adf7e46abf8453069
msyafiqq <muhammad.syafiq@beautyfulminds.com.sg>
2021-06-24 06:57:39 +0700

    register bulk law local

--- lib/di/module/data_layer/data_source/local_datasource_module.dart
+++ lib/di/module/data_layer/data_source/local_datasource_module.dart
@@ -1,9 +1,12 @@
+import 'package:hukum_pro/arch/data/data_source/local/contract/bulk_laws_local_datasource.dart';
 import 'package:hukum_pro/arch/data/data_source/local/contract/law_local_datasource.dart';
 import 'package:hukum_pro/arch/data/data_source/local/contract/law_menu_order_local_datasource.dart';
 import 'package:hukum_pro/arch/data/data_source/local/contract/law_year_local_datasource.dart';
 import 'package:hukum_pro/arch/data/data_source/local/contract/version_local_datasource.dart';
 import 'package:hukum_pro/arch/data/data_source/local/impl/cache_shared_preferences.dart';
+import 'package:hukum_pro/arch/data/data_source/local/impl/disk_path_provider.dart';
 import 'package:hukum_pro/arch/data/data_source/local/impl/object_box_database_storage.dart';
+import 'package:hukum_pro/arch/infrastructure/app/platform_identifier.dart';
 import 'package:hukum_pro/arch/infrastructure/local_database/object_box/store_provider.dart';
 import 'package:kiwi/kiwi.dart';
 import 'package:shared_preferences/shared_preferences.dart';
@@ -34,5 +37,10 @@ class LocalDatasourceModule {
         (c) => c<ObjectBoxDatabaseStorage>());
     container.registerSingleton<LawYearLocalDatasource>(
         (c) => c<ObjectBoxDatabaseStorage>());
+
+    container
+        .registerSingleton((c) => DiskPathProvider(c<PlatformIdentifier>()));
+    container.registerSingleton<BulkLawsLocalDatasource>(
+        (c) => c<DiskPathProvider>());
   }
 }
