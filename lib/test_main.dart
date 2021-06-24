5461636b20c1d3e1422f25338bafa29d727a4eb7
msyafiqq <muhammad.syafiq@beautyfulminds.com.sg>
2021-06-24 07:12:00 +0700

    remove di test

--- lib/test_main.dart
+++ lib/test_main.dart
@@ -1,28 +1,8 @@
-import 'package:firebase_database/firebase_database.dart';
-import 'package:firebase_storage/firebase_storage.dart';
-import 'package:flutter/material.dart';
-
 // Import the firebase_core plugin
 import 'package:firebase_core/firebase_core.dart';
-import 'package:hukum_pro/arch/data/data_source/local/contract/law_local_datasource.dart';
-import 'package:hukum_pro/arch/data/data_source/local/contract/law_menu_order_local_datasource.dart';
-import 'package:hukum_pro/arch/data/data_source/local/contract/law_year_local_datasource.dart';
-import 'package:hukum_pro/arch/data/data_source/local/contract/version_local_datasource.dart';
-import 'package:hukum_pro/arch/data/data_source/local/impl/cache_shared_preferences.dart';
-import 'package:hukum_pro/arch/data/data_source/local/impl/object_box_database_storage.dart';
-import 'package:hukum_pro/arch/data/data_source/remote/contract/bulk_laws_remote_datasource.dart';
-import 'package:hukum_pro/arch/data/data_source/remote/contract/law_menu_order_remote_datasource.dart';
-import 'package:hukum_pro/arch/data/data_source/remote/contract/version_remote_datasource.dart';
-import 'package:hukum_pro/arch/data/data_source/remote/impl/firebase_cloud_database.dart';
-import 'package:hukum_pro/arch/data/data_source/remote/impl/firebase_cloud_storage.dart';
-import 'package:hukum_pro/arch/infrastructure/app/platform_identifier.dart';
-import 'package:hukum_pro/arch/infrastructure/local_database/object_box/store_provider.dart';
-import 'package:hukum_pro/di/contract/object_resolver.dart';
+import 'package:flutter/material.dart';
 import 'package:hukum_pro/di/root_injector.dart';
-import 'package:kiwi/kiwi.dart';
-import 'package:objectbox/objectbox.dart';
 import 'package:path_provider/path_provider.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -74,7 +54,6 @@ class _AppState extends State<App> {
           print("hasData = ${snapshot.hasData}");
           print(snapshot.data);
           print(Firebase.app());
-          checkKiwi();
         }
 
         // Otherwise, show something whilst waiting for initialization to complete
@@ -94,53 +73,5 @@ class _AppState extends State<App> {
         );
       },
     );
-  }
-
-  void checkKiwi() {
-    KiwiContainer container = KiwiContainer();
-    print('checkKiwi');
-    checkObject(Firebase.app());
-    checkObject(container.resolve<FirebaseApp>());
-    checkObject(container.resolve<FirebaseApp>());
-    checkObject(container.resolve<FirebaseDatabase>());
-    checkObject(container.resolve<FirebaseDatabase>());
-    checkObject(container.resolve<FirebaseStorage>());
-    checkObject(container.resolve<FirebaseStorage>());
-    checkObject(container.resolve<FirebaseCloudDatabase>());
-    checkObject(container.resolve<FirebaseCloudDatabase>());
-    checkObject(container.resolve<VersionRemoteDatasource>());
-    checkObject(container.resolve<VersionRemoteDatasource>());
-    checkObject(container.resolve<LawMenuOrderRemoteDatasource>());
-    checkObject(container.resolve<LawMenuOrderRemoteDatasource>());
-    checkObject(container.resolve<FirebaseCloudStorage>());
-    checkObject(container.resolve<FirebaseCloudStorage>());
-    checkObject(container.resolve<BulkLawsRemoteDatasource>());
-    checkObject(container.resolve<BulkLawsRemoteDatasource>());
-    checkObject(container.resolve<Future<SharedPreferences>>());
-    checkObject(container.resolve<Future<SharedPreferences>>());
-    checkObject(container.resolve<CacheSharedPreferences>());
-    checkObject(container.resolve<CacheSharedPreferences>());
-    checkObject(container.resolve<VersionLocalDatasource>());
-    checkObject(container.resolve<VersionLocalDatasource>());
-    checkObject(container.resolve<LawMenuOrderLocalDatasource>());
-    checkObject(container.resolve<LawMenuOrderLocalDatasource>());
-    checkObject(container.resolve<PlatformIdentifier>());
-    checkObject(container.resolve<PlatformIdentifier>());
-    checkObject(container.resolve<Future<Store>>());
-    checkObject(container.resolve<Future<Store>>());
-    checkObject(container.resolve<ObjectResolver>());
-    checkObject(container.resolve<ObjectResolver>());
-    checkObject(container.resolve<StoreProvider>());
-    checkObject(container.resolve<StoreProvider>());
-    checkObject(container.resolve<ObjectBoxDatabaseStorage>());
-    checkObject(container.resolve<ObjectBoxDatabaseStorage>());
-    checkObject(container.resolve<LawLocalDatasource>());
-    checkObject(container.resolve<LawLocalDatasource>());
-    checkObject(container.resolve<LawYearLocalDatasource>());
-    checkObject(container.resolve<LawYearLocalDatasource>());
-  }
-
-  void checkObject(Object x) {
-    print("${x.hashCode} - $x");
   }
 }
