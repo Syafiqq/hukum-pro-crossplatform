5409004d84bde3a5711c7472e203bfbf47cd7e87
msyafiqq <muhammad.syafiq@beautyfulminds.com.sg>
2021-06-27 17:14:29 +0700

    add check local version state

---
+++ lib/arch/presentation/view_model/state/check_local_version_state.dart
@@ -0,0 +1,8 @@
+import 'package:hukum_pro/arch/domain/entity/version/check_local_version_state.dart'
+    as DomainCheckLocalVersionState;
+
+class CheckLocalVersionState {
+  final DomainCheckLocalVersionState.CheckLocalVersionState? status;
+
+  CheckLocalVersionState(this.status);
+}
