9fc73d7e25c2fb7785d70ffdf2c1900a92eddd22
msyafiqq <muhammad.syafiq@beautyfulminds.com.sg>
2021-06-28 20:44:32 +0700

    update view

--- lib/arch/presentation/ui/splash_screen.dart
+++ lib/arch/presentation/ui/splash_screen.dart
@@ -9,32 +9,57 @@ class SplashScreen extends StatelessWidget {
       create: (context) =>
           KiwiObjectResolver.getInstance().getCheckLocalVersionCubit()
             ..checkVersion(),
-      child: Container(
-        child: Column(
-          children: [
-            Expanded(
-              child: Stack(
-                children: [
-                  Align(
-                    alignment: Alignment.lerp(
-                            Alignment.topCenter, Alignment.bottomCenter, 0.4) ??
-                        Alignment.center,
-                    child: Image(
-                      image:
-                          AssetImage('res/images/ic_hukum_pro_logo_2_192.png'),
+      child: Column(
+        children: [
+          Expanded(
+            child: Stack(
+              children: [
+                Align(
+                  alignment: Alignment.lerp(
+                          Alignment.topCenter, Alignment.bottomCenter, 0.4) ??
+                      Alignment.center,
+                  child: Image(
+                    image: AssetImage('res/images/ic_hukum_pro_logo_2_192.png'),
+                    color: Color(0xFF313131),
+                    width: 128,
+                    height: 128,
+                  ),
+                ),
+                Align(
+                  alignment: Alignment.bottomCenter,
+                  child: Text(
+                    'Hukum Pro',
+                    style: TextStyle(
+                      fontFamily: 'Montserrat',
+                      fontWeight: FontWeight.w800,
+                      fontSize: 28,
                       color: Color(0xFF313131),
-                      width: 128,
-                      height: 128,
                     ),
                   ),
-                  Align(
-                    alignment: Alignment.bottomCenter,
+                )
+              ],
+            ),
+          ),
+          Container(
+            child: Padding(
+              padding: EdgeInsets.fromLTRB(8, 16, 8, 8),
+              child: Column(
+                children: [
+                  SizedBox(
+                    width: 120,
+                    child: LinearProgressIndicator(),
+                  ),
+                  Visibility(
+                    visible: false,
+                    maintainState: true,
+                    maintainAnimation: true,
+                    maintainSize: true,
                     child: Text(
-                      'Hukum Pro',
+                      'Check Version',
                       style: TextStyle(
-                        fontFamily: 'Montserrat',
-                        fontWeight: FontWeight.w800,
-                        fontSize: 28,
+                        fontFamily: 'Khula',
+                        fontWeight: FontWeight.w600,
+                        fontSize: 10,
                         color: Color(0xFF313131),
                       ),
                     ),
@@ -42,36 +67,8 @@ class SplashScreen extends StatelessWidget {
                 ],
               ),
             ),
-            Container(
-              child: Padding(
-                padding: EdgeInsets.fromLTRB(8, 16, 8, 8),
-                child: Column(
-                  children: [
-                    SizedBox(
-                      width: 120,
-                      child: LinearProgressIndicator(),
-                    ),
-                    Visibility(
-                      visible: false,
-                      maintainState: true,
-                      maintainAnimation: true,
-                      maintainSize: true,
-                      child: Text(
-                        'Check Version',
-                        style: TextStyle(
-                          fontFamily: 'Khula',
-                          fontWeight: FontWeight.w600,
-                          fontSize: 10,
-                          color: Color(0xFF313131),
-                        ),
-                      ),
-                    )
-                  ],
-                ),
-              ),
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
