216b21a6ec106188faaa484030a11a12e3bafa08
msyafiqq <muhammad.syafiq@beautyfulminds.com.sg>
2021-07-03 15:55:08 +0700

    Add app color

---
+++ lib/common/ui/app_color.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class AppColor {
+  static const primary = Color(0xFF1565c0);
+  static const primaryDark = Color(0xFF5e92f3);
+  static const primaryLight = Color(0xFF003c8f);
+  static const secondary = Color(0xFFff8f00);
+  static const secondaryLight = Color(0xFFffc046);
+  static const secondaryDark = Color(0xFFc56000);
+
+  static const success = Color(0xFF9FE6A0);
+  static const warning = Color(0xFFFFDB83);
+  static const danger = Color(0xFFFD777D);
+  static const info = Color(0xFF00B5FF);
+
+  static const textPrimary = Colors.white;
+  static const textSecondary = Colors.black;
+  static const textOnLightPrimary = Color(0xFF222222);
+  static const textOnLightSecondary = Color(0xFF515151);
+}
