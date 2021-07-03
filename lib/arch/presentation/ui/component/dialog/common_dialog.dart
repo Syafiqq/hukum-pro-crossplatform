f2ee767a03e8729a73fcd235d42fb0efbf2b19c1
msyafiqq <muhammad.syafiq@beautyfulminds.com.sg>
2021-07-03 21:15:00 +0700

    change to stateless widget

--- lib/arch/presentation/ui/component/dialog/common_dialog.dart
+++ lib/arch/presentation/ui/component/dialog/common_dialog.dart
@@ -4,7 +4,7 @@ import 'package:hukum_pro/common/ui/app_color.dart';
 const double _kPadding = 20;
 const double _kAvatarRadius = 45;
 
-class CommonDialog extends StatefulWidget {
+class CommonDialog extends StatelessWidget {
   final String tmpTitle, tmpDescription, tmpText;
   final Image tmpImg;
 
@@ -15,11 +15,6 @@ class CommonDialog extends StatefulWidget {
       {this.title, this.closable})
       : super();
 
-  @override
-  _CommonDialogState createState() => _CommonDialogState();
-}
-
-class _CommonDialogState extends State<CommonDialog> {
   @override
   Widget build(BuildContext context) {
     return Dialog(
@@ -36,7 +31,7 @@ class _CommonDialogState extends State<CommonDialog> {
               SizedBox.shrink(),
             ],
           ),
-          this.widget.closable ?? false
+          this.closable ?? false
               ? Row(
                   mainAxisAlignment: MainAxisAlignment.end,
                   children: [
@@ -52,71 +47,6 @@ class _CommonDialogState extends State<CommonDialog> {
               : SizedBox.shrink(),
         ],
       ),
-    );
-  }
-
-  contentBox(context) {
-    return Stack(
-      children: <Widget>[
-        Container(
-          padding: EdgeInsets.only(
-              left: _kPadding,
-              top: _kAvatarRadius + _kPadding,
-              right: _kPadding,
-              bottom: _kPadding),
-          margin: EdgeInsets.only(top: _kAvatarRadius),
-          decoration: BoxDecoration(
-              shape: BoxShape.rectangle,
-              color: Colors.white,
-              borderRadius: BorderRadius.circular(_kPadding),
-              boxShadow: [
-                BoxShadow(
-                    color: Colors.black, offset: Offset(0, 10), blurRadius: 10),
-              ]),
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: <Widget>[
-              Text(
-                widget.tmpTitle,
-                style: TextStyle(fontSize: 22, fontWeight: FontWeight.w600),
-              ),
-              SizedBox(
-                height: 15,
-              ),
-              Text(
-                widget.tmpDescription,
-                style: TextStyle(fontSize: 14),
-                textAlign: TextAlign.center,
-              ),
-              SizedBox(
-                height: 22,
-              ),
-              Align(
-                alignment: Alignment.bottomRight,
-                child: TextButton(
-                    onPressed: () {
-                      Navigator.of(context).pop();
-                    },
-                    child: Text(
-                      widget.tmpText,
-                      style: TextStyle(fontSize: 18),
-                    )),
-              ),
-            ],
-          ),
-        ),
-        Positioned(
-          left: _kPadding,
-          right: _kPadding,
-          child: CircleAvatar(
-            backgroundColor: Colors.transparent,
-            radius: _kAvatarRadius,
-            child: ClipRRect(
-                borderRadius: BorderRadius.all(Radius.circular(_kAvatarRadius)),
-                child: widget.tmpImg),
-          ),
-        ),
-      ],
     );
   }
 }
