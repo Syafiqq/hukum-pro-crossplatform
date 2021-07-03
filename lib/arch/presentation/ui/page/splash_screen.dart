1818a51a29137061ee6e73c47b11273f4002d5a5
msyafiqq <muhammad.syafiq@beautyfulminds.com.sg>
2021-07-03 18:17:59 +0700

    fixing splash screen

--- lib/arch/presentation/ui/page/splash_screen.dart
+++ lib/arch/presentation/ui/page/splash_screen.dart
@@ -1,74 +1,17 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:hukum_pro/arch/presentation/view_model/cubit/check_local_version_cubit.dart';
-import 'package:hukum_pro/arch/presentation/view_model/state/check_local_version_ui_state.dart';
-import 'package:hukum_pro/common/ui/app_color.dart';
-import 'package:hukum_pro/common/ui/app_font.dart';
-import 'package:hukum_pro/di/impl/kiwi_object_resolver.dart';
+import 'package:hukum_pro/arch/presentation/ui/view/splash_view.dart';
 
 class SplashScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (context) =>
-          KiwiObjectResolver.getInstance().getCheckLocalVersionCubit()
-            ..checkVersion(),
-      child: Column(
-        children: [
-          Expanded(
-            child: Align(
-              alignment: Alignment.lerp(
-                      Alignment.topCenter, Alignment.bottomCenter, 0.4) ??
-                  Alignment.center,
-              child: Image(
-                image: AssetImage('res/images/ic_hukum_pro_logo_2_192.png'),
-                color: Color(0xFF313131),
-                width: 128,
-                height: 128,
-              ),
-            ),
-          ),
-          Text(
-            'Hukum Pro',
-            style: FontTitle.extraBold.font(28, AppColor.textOnLightPrimary),
-          ),
-          Container(
-            height: 36,
-            child:
-                BlocBuilder<CheckLocalVersionCubit, CheckLocalVersionUiState>(
-              builder: (context, state) {
-                switch (state.status) {
-                  case CheckLocalVersionUiStatus.initial:
-                    return SizedBox.shrink();
-                  case CheckLocalVersionUiStatus.loading:
-                    return buildProgress(context, 'Check Version');
-                  case CheckLocalVersionUiStatus.success:
-                    return SizedBox.shrink();
-                  case CheckLocalVersionUiStatus.failure:
-                    return SizedBox.shrink();
-                }
-              },
-            ),
-          ),
-        ],
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
       ),
-    );
-  }
-
-  Widget buildProgress(BuildContext context, [String text = '']) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      crossAxisAlignment: CrossAxisAlignment.center,
-      children: [
-        SizedBox(
-          width: 120,
-          child: LinearProgressIndicator(),
-        ),
-        Text(
-          text,
-          style: FontSubtitle.semiBold.font(10, AppColor.textOnLightPrimary),
-        ),
-      ],
+      home: Scaffold(
+        body: SplashView(),
+      ),
     );
   }
 }
