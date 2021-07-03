0b70f12aab5b4674711619497718d3a13ff374f8
msyafiqq <muhammad.syafiq@beautyfulminds.com.sg>
2021-07-03 20:10:54 +0700

    add closable

--- lib/arch/presentation/ui/component/dialog/common_dialog.dart
+++ lib/arch/presentation/ui/component/dialog/common_dialog.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:hukum_pro/common/ui/app_color.dart';
 
 const double _kPadding = 20;
 const double _kAvatarRadius = 45;
@@ -8,9 +9,10 @@ class CommonDialog extends StatefulWidget {
   final Image tmpImg;
 
   final String? title;
+  final bool? closable;
 
   CommonDialog(this.tmpTitle, this.tmpDescription, this.tmpText, this.tmpImg,
-      {this.title})
+      {this.title, this.closable})
       : super();
 
   @override
@@ -24,10 +26,27 @@ class _CommonDialogState extends State<CommonDialog> {
       shape: RoundedRectangleBorder(
         borderRadius: BorderRadius.circular(8),
       ),
-      elevation: 0,
-      backgroundColor: Colors.white,
-      child: Container(
-        height: 100,
+      elevation: 24,
+      backgroundColor: AppColor.background,
+      child: Stack(
+        children: [
+          Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [SizedBox.shrink()],
+          ),
+          this.widget.closable ?? false
+              ? Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    IconButton(
+                      icon: const Icon(Icons.close),
+                      color: AppColor.secondary,
+                      onPressed: () {},
+                    )
+                  ],
+                )
+              : SizedBox.shrink(),
+        ],
       ),
     );
   }
