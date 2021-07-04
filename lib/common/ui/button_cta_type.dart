e1fdc497816d4cf3e780fd915ba6fbda66e792f6
msyafiqq <muhammad.syafiq@beautyfulminds.com.sg>
2021-07-04 10:12:06 +0700

    add button cta type

---
+++ lib/common/ui/button_cta_type.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'button_cta_type.freezed.dart';
+
+@freezed
+class ButtonCtaType with _$ButtonCtaType {
+  const factory ButtonCtaType.outline(bool disabled) = ButtonCtaTypeOutline;
+
+  const factory ButtonCtaType.solid(bool disabled, Color color) =
+      ButtonCtaTypeSolid;
+}
