6a9510db8a18f525ad338cafb6997a4510f812c6
msyafiqq <muhammad.syafiq@beautyfulminds.com.sg>
2021-07-04 13:20:33 +0700

    Change to bloc consumer

--- lib/arch/presentation/ui/view/splash_view.dart
+++ lib/arch/presentation/ui/view/splash_view.dart
@@ -1,13 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:hukum_pro/arch/domain/entity/misc/version_entity.dart';
 import 'package:hukum_pro/arch/domain/entity/version/check_local_version_state.dart';
 import 'package:hukum_pro/arch/domain/use_case/check_version_first_time_use_case.dart';
+import 'package:hukum_pro/arch/presentation/ui/component/dialog/common_dialog.dart';
 import 'package:hukum_pro/arch/presentation/view_model/cubit/check_local_version_cubit.dart';
 import 'package:hukum_pro/arch/presentation/view_model/state/check_local_version_ui_state.dart';
 import 'package:hukum_pro/common/exception/defined_exception.dart';
 import 'package:hukum_pro/common/ui/app_color.dart';
 import 'package:hukum_pro/common/ui/app_font.dart';
+import 'package:hukum_pro/common/ui/button_cta_type.dart';
 
 class SplashView extends StatelessWidget {
   @override
@@ -38,7 +39,29 @@ class SplashView extends StatelessWidget {
           Container(
             height: 36,
             child:
-                BlocBuilder<CheckLocalVersionCubit, CheckLocalVersionUiState>(
+                BlocConsumer<CheckLocalVersionCubit, CheckLocalVersionUiState>(
+              listener: (context, state) {
+                switch (state.status) {
+                  case CheckLocalVersionUiStatus.failure:
+                    CommonDialog.show(context,
+                            description: "Failed to initialize app",
+                            primaryAction: "Okay",
+                            primaryStyle: ButtonCtaType.solid(
+                              false,
+                              AppColor.secondary,
+                              AppColor.textSecondary,
+                            ),
+                            isClosable: false,
+                            dismissOnTouchOutside: false)
+                        .then(
+                      (value) =>
+                          context.read<CheckLocalVersionCubit>().checkVersion(),
+                    );
+                    break;
+                  default:
+                    break;
+                }
+              },
               builder: (context, state) {
                 switch (state.status) {
                   case CheckLocalVersionUiStatus.initial:
