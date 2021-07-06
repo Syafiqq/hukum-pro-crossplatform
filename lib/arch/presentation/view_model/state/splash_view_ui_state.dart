7c9ea8bf687979c831922d51d51e5bea089732ae
msyafiqq <muhammad.syafiq@beautyfulminds.com.sg>
2021-07-07 06:45:52 +0700

    fixing splash view ui state

--- lib/arch/presentation/view_model/state/splash_view_ui_state.dart
+++ lib/arch/presentation/view_model/state/splash_view_ui_state.dart
@@ -6,15 +6,20 @@ part 'splash_view_ui_state.freezed.dart';
 @freezed
 class SplashViewUiState with _$SplashViewUiState {
   const factory SplashViewUiState.initial() = InitialState;
-  const factory SplashViewUiState.loadingCheckVersion() = LoadingCheckVersion;
-  const factory SplashViewUiState.versionPresent(VersionEntity version) =
-      VersionPresent;
-  const factory SplashViewUiState.needInitializeApp(VersionEntity version) =
-      VersionInitialize;
-  const factory SplashViewUiState.checkVersionFailed(Exception exception) =
-      VersionCheckFailed;
-  const factory SplashViewUiState.loadingInitializeApp() = LoadingInitializeApp;
+  const factory SplashViewUiState.versionLoading() = VersionLoading;
+  const factory SplashViewUiState.versionPresent(
+    VersionEntity version,
+  ) = VersionPresent;
+  const factory SplashViewUiState.checkVersionSuccess(
+    VersionEntity version,
+  ) = VersionCheckSuccess;
+  const factory SplashViewUiState.checkVersionFailed(
+    Exception exception,
+  ) = VersionCheckFailed;
+  const factory SplashViewUiState.initializeAppLoading() = InitializeLoading;
   const factory SplashViewUiState.initializeAppSuccess() = InitializeSuccess;
-  const factory SplashViewUiState.initializeAppFailed(Exception exception) =
-      InitializeFailed;
+  const factory SplashViewUiState.initializeAppFailed(
+    Exception exception,
+    VersionEntity version,
+  ) = InitializeFailed;
 }
