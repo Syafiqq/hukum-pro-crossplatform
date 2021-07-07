e1f142bffc54c2e537bdda4ec2f03687ae15d239
msyafiqq <muhammad.syafiq@beautyfulminds.com.sg>
2021-07-07 07:06:35 +0700

    rename function

--- lib/arch/presentation/view_model/state/check_local_version_and_initialize_state.dart
+++ lib/arch/presentation/view_model/state/check_local_version_and_initialize_state.dart
@@ -1,7 +1,7 @@
 import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:hukum_pro/arch/domain/entity/misc/version_entity.dart';
 
-part 'splash_view_ui_state.freezed.dart';
+part 'check_local_version_and_initialize_state.freezed.dart';
 
 @freezed
 class SplashViewUiState with _$SplashViewUiState {
@@ -9,17 +9,14 @@ class SplashViewUiState with _$SplashViewUiState {
   const factory SplashViewUiState.versionLoading() = VersionLoading;
   const factory SplashViewUiState.versionPresent(
     VersionEntity version,
-  ) = VersionPresent;
-  const factory SplashViewUiState.checkVersionSuccess(
+  ) = VersionLocalPresent;
+  const factory SplashViewUiState.versionNotExistButRemote(
     VersionEntity version,
-  ) = VersionCheckSuccess;
-  const factory SplashViewUiState.checkVersionFailed(
-    Exception exception,
-  ) = VersionCheckFailed;
+  ) = VersionLocalNotPresent;
+  const factory SplashViewUiState.checkVersionFailed() = VersionCheckFailed;
   const factory SplashViewUiState.initializeAppLoading() = InitializeLoading;
   const factory SplashViewUiState.initializeAppSuccess() = InitializeSuccess;
   const factory SplashViewUiState.initializeAppFailed(
-    Exception exception,
     VersionEntity version,
   ) = InitializeFailed;
 }
