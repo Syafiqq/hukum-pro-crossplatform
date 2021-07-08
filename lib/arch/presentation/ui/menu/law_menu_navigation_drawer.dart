373d17c61bd97832a0d11c022f5ea7302ac8055b
msyafiqq <muhammad.syafiq@beautyfulminds.com.sg>
2021-07-08 21:15:48 +0700

    fixing viewˆ

--- lib/arch/presentation/ui/menu/law_menu_navigation_drawer.dart
+++ lib/arch/presentation/ui/menu/law_menu_navigation_drawer.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:hukum_pro/arch/presentation/ui/view/law_menu_navigation_view.dart';
 
 class LawMenuNavigationDrawer extends StatelessWidget {
   LawMenuNavigationDrawer() : super();
@@ -6,71 +7,5 @@ class LawMenuNavigationDrawer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Drawer(child: LawMenuNavigationView());
-  }
-}
-
-class LawMenuNavigationView extends StatelessWidget {
-  final int _selectedDestination = 0;
-
-  LawMenuNavigationView() : super();
-
-  @override
-  Widget build(BuildContext context) {
-    return Drawer(
-      child: ListView(
-        // Important: Remove any padding from the ListView.
-        padding: EdgeInsets.zero,
-        children: <Widget>[
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Text(
-              'Header',
-            ),
-          ),
-          Divider(
-            height: 1,
-            thickness: 1,
-          ),
-          ListTile(
-            leading: Icon(Icons.favorite),
-            title: Text('Item 1'),
-            selected: _selectedDestination == 0,
-            onTap: () => selectDestination(0),
-          ),
-          ListTile(
-            leading: Icon(Icons.delete),
-            title: Text('Item 2'),
-            selected: _selectedDestination == 1,
-            onTap: () => selectDestination(1),
-          ),
-          ListTile(
-            leading: Icon(Icons.label),
-            title: Text('Item 3'),
-            selected: _selectedDestination == 2,
-            onTap: () => selectDestination(2),
-          ),
-          Divider(
-            height: 1,
-            thickness: 1,
-          ),
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Text(
-              'Label',
-            ),
-          ),
-          ListTile(
-            leading: Icon(Icons.bookmark),
-            title: Text('Item A'),
-            selected: _selectedDestination == 3,
-            onTap: () => selectDestination(3),
-          ),
-        ],
-      ),
-    );
-  }
-
-  void selectDestination(int index) {
-    print('Select destination $index');
   }
 }
