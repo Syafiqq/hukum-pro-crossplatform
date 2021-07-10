b9d878818dd72e6c48c49b9e8ac2857699c7dfa4
msyafiqq <muhammad.syafiq@beautyfulminds.com.sg>
2021-07-10 21:23:22 +0700

    register cubit

--- lib/di/module/presentation_layer/cubit_module.dart
+++ lib/di/module/presentation_layer/cubit_module.dart
@@ -1,6 +1,8 @@
+import 'package:hukum_pro/arch/domain/repository/law_menu_order_repository.dart';
 import 'package:hukum_pro/arch/domain/use_case/check_version_first_time_use_case.dart';
 import 'package:hukum_pro/arch/domain/use_case/reinitialize_whole_data_use_case.dart';
 import 'package:hukum_pro/arch/presentation/view_model/cubit/check_local_version_and_initialize_cubit.dart';
+import 'package:hukum_pro/arch/presentation/view_model/cubit/load_law_menu_cubit.dart';
 import 'package:kiwi/kiwi.dart';
 
 class CubitModule {
@@ -18,6 +20,11 @@ class CubitModule {
       (c) => CheckLocalVersionAndInitializeCubit(
         c.resolve<CheckVersionFirstTimeUseCase>(),
         c.resolve<ReinitializeWholeDataUseCase>(),
+      ),
+    );
+    container.registerFactory<LoadLawMenuCubit>(
+      (c) => LoadLawMenuCubit(
+        c.resolve<LawMenuOrderRepository>(),
       ),
     );
   }
