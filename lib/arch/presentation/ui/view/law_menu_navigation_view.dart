61fbd9ae27c4bb2674fdd94ce0db31fef95c31c0
msyafiqq <muhammad.syafiq@beautyfulminds.com.sg>
2021-07-10 21:04:39 +0700

    FIxing sinkron

--- lib/arch/presentation/ui/view/law_menu_navigation_view.dart
+++ lib/arch/presentation/ui/view/law_menu_navigation_view.dart
@@ -87,40 +87,6 @@ class LawMenuNavigationView extends StatelessWidget {
             height: 1,
             thickness: 1,
           ),
-          ListTile(
-            leading: Icon(Icons.favorite),
-            title: Text('Item 1'),
-            selected: _selectedDestination == 1,
-            onTap: () => selectDestination(0),
-          ),
-          ListTile(
-            leading: Icon(Icons.delete),
-            title: Text('Item 2'),
-            selected: _selectedDestination == 2,
-            onTap: () => selectDestination(1),
-          ),
-          ListTile(
-            leading: Icon(Icons.label),
-            title: Text('Item 3'),
-            selected: _selectedDestination == 3,
-            onTap: () => selectDestination(2),
-          ),
-          Divider(
-            height: 1,
-            thickness: 1,
-          ),
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Text(
-              'Label',
-            ),
-          ),
-          ListTile(
-            leading: Icon(Icons.bookmark),
-            title: Text('Item A'),
-            selected: _selectedDestination == 4,
-            onTap: () => selectDestination(3),
-          ),
         ],
       ),
     );
