9291ba93a51a951f882c877aeae88ba6f06664e3
msyafiqq <muhammad.syafiq@beautyfulminds.com.sg>
2021-07-11 12:50:17 +0700

    fixing has more

--- lib/arch/presentation/view_model/state/law_year_load_state.dart
+++ lib/arch/presentation/view_model/state/law_year_load_state.dart
@@ -1,14 +1,22 @@
 import 'package:freezed_annotation/freezed_annotation.dart';
-import 'package:hukum_pro/arch/data/data_source/local/entity/law_year_entity.dart';
+import 'package:hukum_pro/arch/domain/entity/law/law_year_entity.dart';
 
 part 'law_year_load_state.freezed.dart';
 
+enum LawYearLoadUiState {
+  initial,
+  reset,
+  loading,
+  loadMore,
+  loadSuccess,
+  loadFailed,
+}
+
 @freezed
-class LawYearLoadUiState with _$LawYearLoadUiState {
-  const factory LawYearLoadUiState.initial() = LawYearInitialState;
-  const factory LawYearLoadUiState.loading() = LawYearLoadingState;
-  const factory LawYearLoadUiState.loadingMore() = LawYearLoadMoreState;
-  const factory LawYearLoadUiState.loadSuccess(List<LawYearEntity> lawYears) =
-      LawYearLoadSuccessState;
-  const factory LawYearLoadUiState.loadFailed() = LawYearLoadFailedState;
+class LawYearLoadState with _$LawYearLoadState {
+  factory LawYearLoadState({
+    @Default(LawYearLoadUiState.initial) LawYearLoadUiState state,
+    @Default([]) List<LawYearEntity> lawYears,
+    @Default(true) bool hasMore,
+  }) = _LawYearLoadState;
 }
