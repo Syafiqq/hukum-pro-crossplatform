650b2c6c77745de64b165e3e7ab090c2643de744
msyafiqq <muhammad.syafiq@beautyfulminds.com.sg>
2021-07-11 10:29:55 +0700

    add law screen

--- lib/arch/presentation/ui/page/law_screen.dart
+++ lib/arch/presentation/ui/page/law_screen.dart
@@ -3,6 +3,7 @@ import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hukum_pro/arch/presentation/ui/menu/law_menu_navigation_drawer.dart';
+import 'package:hukum_pro/arch/presentation/ui/view/law_menu_navigation_view.dart';
 import 'package:hukum_pro/di/impl/kiwi_object_resolver.dart';
 
 class LawScreen extends StatelessWidget {
@@ -22,7 +23,11 @@ class LawScreen extends StatelessWidget {
             ),
             320,
           ), // 75% of screen will be occupied
-          child: LawMenuNavigationDrawer(),
+          child: Drawer(
+            child: SafeArea(
+              child: LawMenuNavigationView(),
+            ),
+          ),
         ),
       ),
     );
