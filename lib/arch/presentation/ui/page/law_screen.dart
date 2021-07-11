89f2eb6fda943db70f68a5fc02143f2299979575
msyafiqq <muhammad.syafiq@beautyfulminds.com.sg>
2021-07-11 10:26:22 +0700

    add law screen

---
+++ lib/arch/presentation/ui/page/law_screen.dart
@@ -0,0 +1,30 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hukum_pro/arch/presentation/ui/menu/law_menu_navigation_drawer.dart';
+import 'package:hukum_pro/di/impl/kiwi_object_resolver.dart';
+
+class LawScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (context) {
+        return KiwiObjectResolver.getInstance().getLoadLawMenuCubit()..load();
+      },
+      child: Scaffold(
+        body: Container(),
+        drawer: SizedBox(
+          width: min(
+            max(
+              256,
+              MediaQuery.of(context).size.width * 0.75,
+            ),
+            320,
+          ), // 75% of screen will be occupied
+          child: LawMenuNavigationDrawer(),
+        ),
+      ),
+    );
+  }
+}
