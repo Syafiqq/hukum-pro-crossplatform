25410789282b455dcf7e6a12a81327e5613f2715
msyafiqq <muhammad.syafiq@beautyfulminds.com.sg>
2021-07-11 07:47:21 +0700

    simplify build

--- lib/arch/presentation/ui/view/law_menu_navigation_view.dart
+++ lib/arch/presentation/ui/view/law_menu_navigation_view.dart
@@ -20,81 +20,85 @@ class LawMenuNavigationView extends StatelessWidget {
           // Important: Remove any padding from the ListView.
           padding: EdgeInsets.zero,
           children: <Widget>[
-            Padding(
-              padding: const EdgeInsets.fromLTRB(0, 24, 0, 24),
-              child: Center(
-                child: Row(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    const Image(
-                      image: const AssetImage(
-                        'res/images/ic_hukum_pro_logo_2_48.png',
-                      ),
-                      color: AppColor.textOnLightPrimary,
-                      width: 36,
-                      height: 36,
-                    ),
-                    const SizedBox(width: 4),
-                    Text(
-                      'Hukum Pro',
-                      textAlign: TextAlign.center,
-                      style: AppFontTitle.extraBold.font(
-                        24,
-                        color: AppColor.textOnLightPrimary,
-                      ),
-                    ),
-                  ],
-                ),
+            buildHeader(context),
+            const Divider(height: 1, thickness: 1),
+            buildSearch(context),
+            const Divider(height: 1, thickness: 1),
+            buildSpinner(context),
+            const Divider(height: 1, thickness: 1),
+            buildSync(context),
+            const Divider(height: 1, thickness: 1),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget buildHeader(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(0, 24, 0, 24),
+      child: Center(
+        child: Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            const Image(
+              image: const AssetImage(
+                'res/images/ic_hukum_pro_logo_2_48.png',
               ),
+              color: AppColor.textOnLightPrimary,
+              width: 36,
+              height: 36,
             ),
-            const Divider(
-              height: 1,
-              thickness: 1,
-            ),
-            ListTile(
-              leading: const Icon(Icons.search),
-              title: Text(
-                'Pencarian',
-                style: AppFontContent.regular.font(16),
+            const SizedBox(width: 4),
+            Text(
+              'Hukum Pro',
+              textAlign: TextAlign.center,
+              style: AppFontTitle.extraBold.font(
+                24,
+                color: AppColor.textOnLightPrimary,
               ),
-              onTap: () => selectDestination(0),
-            ),
-            const Divider(
-              height: 1,
-              thickness: 1,
-            ),
-            Padding(
-              padding: EdgeInsets.all(16),
-              child: Center(
-                child: SizedBox(
-                  height: 32,
-                  width: 32,
-                  child: LoadingIndicator(
-                    indicatorType: Indicator.circleStrokeSpin,
-                    color: Colors.black,
-                  ),
-                ),
-              ),
-            ),
-            const Divider(
-              height: 1,
-              thickness: 1,
-            ),
-            ListTile(
-              leading: const Icon(Icons.sync),
-              title: Text(
-                'Sinkron',
-                style: AppFontContent.regular.font(16),
-              ),
-              onTap: () => selectDestination(0),
-            ),
-            const Divider(
-              height: 1,
-              thickness: 1,
             ),
           ],
         ),
       ),
+    );
+  }
+
+  Widget buildSearch(BuildContext context) {
+    return ListTile(
+      leading: const Icon(Icons.search),
+      title: Text(
+        'Pencarian',
+        style: AppFontContent.regular.font(16),
+      ),
+      onTap: () => selectDestination(0),
+    );
+  }
+
+  Widget buildSpinner(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.all(16),
+      child: Center(
+        child: SizedBox(
+          height: 32,
+          width: 32,
+          child: LoadingIndicator(
+            indicatorType: Indicator.circleStrokeSpin,
+            color: Colors.black,
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget buildSync(BuildContext context) {
+    return ListTile(
+      leading: const Icon(Icons.sync),
+      title: Text(
+        'Sinkron',
+        style: AppFontContent.regular.font(16),
+      ),
+      onTap: () => selectDestination(0),
     );
   }
 
