661a872617b8772eb9abfb239949b08691d4cfe4
msyafiqq <muhammad.syafiq@beautyfulminds.com.sg>
2021-09-19 13:29:14 +0700

    Register cubit

---
+++ lib/arch/presentation/ui/view/law/law_year_list_view.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hukum_pro/di/impl/kiwi_object_resolver.dart';
+
+class LawYearListView extends StatelessWidget {
+  final String lawId;
+
+  LawYearListView(this.lawId) : super();
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (BuildContext context) {
+        return KiwiObjectResolver.getInstance().getLoadLawYearCubit()
+          ..resetAndLoad(lawId);
+      },
+      child: Container(),
+    );
+  }
+}
