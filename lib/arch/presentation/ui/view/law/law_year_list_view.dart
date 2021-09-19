9994a3c80a40121131a949081ea249a51858a136
msyafiqq <muhammad.syafiq@beautyfulminds.com.sg>
2021-09-19 13:52:17 +0700

    add spinner

--- lib/arch/presentation/ui/view/law/law_year_list_view.dart
+++ lib/arch/presentation/ui/view/law/law_year_list_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hukum_pro/arch/domain/entity/law/law_year_entity.dart';
+import 'package:hukum_pro/arch/presentation/view_model/state/law_year_load_state.dart';
 import 'package:hukum_pro/di/impl/kiwi_object_resolver.dart';
 
 class LawYearListView extends StatelessWidget {
@@ -15,7 +16,32 @@ class LawYearListView extends StatelessWidget {
         return KiwiObjectResolver.getInstance().getLoadLawYearCubit()
           ..resetAndLoad(lawId);
       },
-      child: Container(),
+      child: BlocBuilder<LoadLawYearCubit, LawYearLoadState>(
+        builder: (context, state) {
+          switch (state.state) {
+            case LawYearLoadUiState.loading:
+              return buildSpinner(context);
+            default:
+              return Container();
+          }
+        },
+      ),
+    );
+  }
+
+  Widget buildSpinner(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.all(16),
+      child: Center(
+        child: SizedBox(
+          height: 32,
+          width: 32,
+          child: LoadingIndicator(
+            indicatorType: Indicator.circleStrokeSpin,
+            colors: [Colors.black],
+          ),
+        ),
+      ),
     );
   }
 }
