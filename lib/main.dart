2a44ef6c2f9b9388c64a32b9df9e2b7c3bff7e65
msyafiqq <muhammad.syafiq@geniebook.com>
2021-10-10 07:28:10 +0700

    update app state

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,41 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:hukum_pro/di/root_injector.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
-  runApp(MyApp());
+  runApp(App());
 }
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class App extends StatefulWidget {
+  // Create the initialization Future outside of `build`:
+  @override
+  _AppState createState() => _AppState();
+}
+
+class _AppState extends State<App> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.amber,
-      ),
-      home: Scaffold(
-        body: Container(),
-      ),
+    return FutureBuilder(
+      // Initialize FlutterFire:
+      future: (() async {
+        await Firebase.initializeApp();
+        RootInjector().build();
+      })(),
+      builder: (context, snapshot) {
+        // Check for errors
+        if (snapshot.hasError) {
+          return Container(color: Colors.red);
+        }
+
+        // Once complete, show your application
+        if (snapshot.connectionState == ConnectionState.done) {
+          return Container(color: Colors.blue);
+        }
+
+        // Otherwise, show something whilst waiting for initialization to complete
+        return Container(color: Colors.green);
+      },
     );
   }
 }
