643be9cafa24cb46c2b13716582a7c7b5e584f6e
msyafiqq <muhammad.syafiq@geniebook.com>
2021-10-10 07:07:58 +0700

    update pslash view

--- lib/arch/presentation/ui/page/splash_screen.dart
+++ lib/arch/presentation/ui/page/splash_screen.dart
@@ -4,14 +4,6 @@ import 'package:hukum_pro/arch/presentation/ui/view/splash_view.dart';
 class SplashScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: Scaffold(
-        body: SplashView(),
-      ),
-    );
+    return SplashView();
   }
 }
